c8b79c037f1c2bf40f2d648a9b39b38dafd75677
nnn-revo2012 <nnn_revo2012@yahoo.co.jp>
2019-07-10 18:07:49 +0900

    HTMLネイティブ録画2

--- NicoNamaRokuga/Record/RecHtml.cs
+++ NicoNamaRokuga/Record/RecHtml.cs
@@ -44,10 +44,12 @@ namespace NicoNamaRokuga.Rec
     {
         public string Status { set; get; }
         public string Error { set; get; }
+        public string BaseUrl { set; get; }
         public ICollection<Segment> Seg { private set; get; }
         public string NextTime { set; get; }
         public string Format { set; get; }
         public string WithoutFormat { set; get; }
+        public int SeqNo { set; get; }
         public float Position { set; get; }
 
         public SegmentInfo()
@@ -60,7 +62,6 @@ namespace NicoNamaRokuga.Rec
 
     public class Segment
     {
-        public int SeqNo { set; get; }
         public float ExtInfo { set; get; }
         public string sUrl { set; get; }
     }
@@ -94,7 +95,6 @@ namespace NicoNamaRokuga.Rec
         public bool IsDebug { get; set; }
 
         public volatile int PsStatus = -1; //実行ファイルの状態
-        private Process _ps = null;
 
         private NicoNetComment _nNetComment = null;   //WebSocket(Comment)
         private BroadCastInfo _bci = null;
@@ -122,23 +122,40 @@ namespace NicoNamaRokuga.Rec
             this.Dispose();
         }
 
-        public void ExecPs(string masterfile, string outfile)
+        public async Task ExecPs(string masterfile, string outfile)
         {
 
+            PsStatus = 0; //実行中
             // masterファイルをGet
-            var pli = GetMasterM3u8Async(masterfile, "");
-
+            var pli = await GetMasterM3u8Async(masterfile, "");
+            if (pli.Status != "Ok" || pli.Player.Count() <= 0) EndPs();
             // playerファイルをGet
+            var sgi = await GetPlayerM3u8Async(masterfile, "");
+            if (sgi.Status != "Ok" || sgi.Seg.Count() <= 0) EndPs();
 
             // 指定秒ごとにSegmentファイルを取得
 
             // ループ
+            EndPs();
 
+        }
+
+        public void EndPs()
+        {
+
+            PsStatus = 2;
+
+        }
+
+        public void BreakProcess(string breakkey)
+        {
+            EndPs();
         }
 
         //master.m3u8からplayer.m3u8のURLを取得
         public async Task<PlayListInfo> GetMasterM3u8Async(string url, string referer)
         {
+            _form.AddExecLog("GetMasterFile");
             var pli = new PlayListInfo();
             pli.Status = "Error";
             pli.Error = "PARAMERROR";
@@ -153,16 +170,22 @@ namespace NicoNamaRokuga.Rec
                 using (var sr = new StreamReader(stream, Encoding.UTF8))
                 {
                     string line;
+                    int bw;
                     while ((line = sr.ReadLine()) != null) // 1行ずつ読み出し。
                     {
+                        _form.AddExecLog(line);
                         if (line.IndexOf("#EXT-X-STREAM-INF") >= 0)
                         {
                             var pi = new PlayerInfo();
-                            int bw;
                             if (int.TryParse(Regex.Match(line, @"BANDWIDTH=(\d+)").Groups[1].Value, out bw))
                                 pi.Bandwidth = bw;
-                            pi.pUrl = pli.BaseUrl + line;
-                            pli.Player.Add(pi);
+                            line = sr.ReadLine();
+                            _form.AddExecLog(line);
+                            if (!string.IsNullOrEmpty(line))
+                            {
+                                pi.pUrl = pli.BaseUrl + line;
+                                pli.Player.Add(pi);
+                            }
                         }
                     }
                 }
@@ -180,30 +203,58 @@ namespace NicoNamaRokuga.Rec
         }
 
         //player.m3u8からsegment情報を取得
-        public async Task<PlayListInfo> GetPlayerM3u8Async(string url, string referer)
+        public async Task<SegmentInfo> GetPlayerM3u8Async(string url, string referer)
         {
-            var pli = new PlayListInfo();
-            pli.Status = "Error";
-            pli.Error = "PARAMERROR";
-            if (string.IsNullOrEmpty(url)) return pli;
+            _form.AddExecLog("GetPlayerFile");
+            var sgi = new SegmentInfo();
+            sgi.Status = "Error";
+            sgi.Error = "PARAMERROR";
+            if (string.IsNullOrEmpty(url)) return sgi;
 
             try
             {
                 var idx = url.IndexOf("player.m3u8");
-                if (idx >= 0) pli.BaseUrl = url.Substring(0, idx);
+                if (idx >= 0) sgi.BaseUrl = url.Substring(0, idx);
                 var str = await _wc.DownloadStringTaskAsync(url);
                 using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(str), false))
                 using (var sr = new StreamReader(stream, Encoding.UTF8))
                 {
+                    int sn;
+                    float ei;
                     string line;
                     while ((line = sr.ReadLine()) != null) // 1行ずつ読み出し。
                     {
-                        var pi = new PlayerInfo();
-                        int bw;
-                        if (int.TryParse(Regex.Match(line, @"BANDWIDTH=(\d+)").Groups[1].Value, out bw))
-                            pi.Bandwidth = bw;
-                        pi.pUrl = pli.BaseUrl + line;
-                        pli.Player.Add(pi);
+                        _form.AddExecLog(line);
+                        var ttt = line.Split(':');
+                        switch (ttt[0])
+                        {
+                            case "#EXT-X-MEDIA-SEQUENCE":
+                                if (int.TryParse(ttt[1], out sn))
+                                    sgi.SeqNo = sn;
+                                break;
+                            case "#CURRENT-POSITION":
+                                if (float.TryParse(ttt[1], out ei))
+                                    sgi.Position = ei;
+                                break;
+                            case "#DMC-CURRENT-POSITION":
+                                if (float.TryParse(ttt[1], out ei))
+                                    sgi.Position = ei;
+                                break;
+                            case "#EXTINF":
+                                var sg = new Segment();
+                                if (float.TryParse(ttt[1], out ei))
+                                    sg.ExtInfo = ei;
+                                line = sr.ReadLine();
+                                _form.AddExecLog(line);
+                                if (!string.IsNullOrEmpty(line))
+                                {
+                                    sg.sUrl = sgi.BaseUrl + line;
+                                    sgi.Seg.Add(sg);
+                                }
+                                break;
+                            default:
+                                break;
+                        }
                     }
                 }
             }
@@ -211,42 +262,33 @@ namespace NicoNamaRokuga.Rec
             {
                 //HttpRequestException
                 DebugWrite.Writeln(nameof(GetPlayerM3u8Async), Ex);
-                pli.Error = Ex.ToString();
-                return pli;
+                sgi.Error = Ex.ToString();
+                return sgi;
             }
-            pli.Status = "Ok";
-            pli.Error = "";
-            return pli;
+            sgi.Status = "Ok";
+            sgi.Error = "";
+            return sgi;
         }
 
         //segmentファイルを取得
-        public async Task<string> GetSegmentAsync(string url, string referer)
+        public async Task<byte[]> GetSegmentAsync(string url, string referer, int seqno, int bw)
         {
-            var result = string.Empty;
-            if (string.IsNullOrEmpty(url)) return result;
+            _form.AddExecLog("GetSegmentFile");
+            byte[] data = null;
+            if (string.IsNullOrEmpty(url)) return data;
 
             try
             {
-                var idx = url.IndexOf("player.m3u8");
-                if (idx >= 0) result = url.Substring(0, idx);
-                var str = await _wc.DownloadStringTaskAsync(url);
-                using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(str), false))
-                using (var sr = new StreamReader(stream, Encoding.UTF8))
-                {
-                    string line;
-                    while ((line = sr.ReadLine()) != null) // 1行ずつ読み出し。
-                    {
-                        if (!line.StartsWith("#")) result += line;
-                    }
-                }
+                //data = await _wc.DownloadDataTaskAsync(url);
+                //var aaa = _wc.ResponseHeaders["Content-Length"].FirstOrDefault();
             }
             catch (Exception Ex) //タイムアウトなど
             {
                 //HttpRequestException
                 DebugWrite.Writeln(nameof(GetSegmentAsync), Ex);
-                return result;
+                return data;
             }
-            return result;
+            return data;
         }
 
 
