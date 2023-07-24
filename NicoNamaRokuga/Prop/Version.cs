01697eb5b367bcf4ac2f057b1a929ab8337bb5b3
nnn-revo2012 <nnn_revo2012@yahoo.co.jp>
2023-07-24 22:45:18 +0900

    Ver 0.1.1.27 - CookieGetter、CookieGetter.Forms をアップデート

--- NicoNamaRokuga/Prop/Version.cs
+++ NicoNamaRokuga/Prop/Version.cs
@@ -4,8 +4,8 @@ namespace NicoNamaRokuga.Prop
 {
     public class Ver
     {
-        public static readonly string Version = "0.1.1.26";
-        public static readonly string VerDate = "2023/04/19";
+        public static readonly string Version = "0.1.1.27";
+        public static readonly string VerDate = "2023/07/28";
 
         public static string GetFullVersion()
         {
